d3521731f088b3bcb3e590d7ca0bc9065a9db592
kjschwoerke <kjschwoerke@yahoo.com>
2020-11-27 14:51:45 -0600

    adjusted css dark mode to change root css vars

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
-const backGround = document.getElementById('background')
-const navBar = document.getElementById("navigation-menu")
-const lDMode = document.getElementById('ldmode')
-const title = document.getElementById('title')
-const buttons = document.getElementsByClassName('splash-buttons')
-const cards = document.getElementsByClassName('work-item-card')
-const contactForm = document.getElementById('contact-form')
-const sendButton = document.getElementById('send-button')
+// const backGround = document.getElementById('background')
+// const navBar = document.getElementById("navigation-menu")
+// const lDMode = document.getElementById('ldmode')
+// const title = document.getElementById('title')
+// const buttons = document.getElementsByClassName('splash-buttons')
+// const cards = document.getElementsByClassName('work-item-card')
+// const contactForm = document.getElementById('contact-form')
+// const sendButton = document.getElementById('send-button')
+
 
 let dark = false
 
 const darkMode = () => {
-    
-    if (dark === false) {
-        backGround.classList.add('dark')
-        navBar.classList.add('dark')
-        title.classList.add('dark')
-        lDMode.classList.add('dark')
-        lDMode.innerHTML = 'Light Mode <img src="./images/sun-solid.svg" alt="Moon Icon to switch to Dark Mode">'
-        contactForm.classList.add('dark')
-        sendButton.classList.add('dark')
+    const root = document.documentElement;
 
-        for(let i = 0; i<buttons.length; i++) {
-            buttons[i].classList.add('dark')
-            cards[i].classList.add('dark')
-        }
-        
+    if (dark === false) {
+    //background color variables
+    root.style.setProperty('--light',  '#2c2c2c')
+    root.style.setProperty('--mid',  '#3a3a3a')
+    root.style.setProperty('--dark',  '#ffffff')
+
+    //text color variables
+    root.style.setProperty('--text-color-light',  '#454545')
+    root.style.setProperty('--text-color-dark',  '#ffffff')
+
         //set dark to True
         dark = true
     } else {
-        backGround.classList.remove('dark')
-        navBar.classList.remove('dark')
-        title.classList.remove('dark')
-        lDMode.classList.remove('dark')
-        lDMode.innerHTML = 'Dark Mode <img src="./images/moon-solid.svg" alt="Moon Icon to switch to Dark Mode">'
-        contactForm.classList.remove('dark')
-        sendButton.classList.remove('dark')
+        //background color variables
+    root.style.setProperty('--light',  '#ffffff')
+    root.style.setProperty('--mid',  '#f5f5f5')
+    root.style.setProperty('--dark',  '#454545')
 
-        for(let i = 0; i<buttons.length; i++) {
-            buttons[i].classList.remove('dark')
-            cards[i].classList.remove('dark')
-        }
+    //text color variables
+    root.style.setProperty('--text-color-light',  '#ffffff')
+    root.style.setProperty('--text-color-dark',  '#454545')
 
+
+    //set dark to False
         dark = false
     }
     
